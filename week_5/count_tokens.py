f97c22a73236331b75f5c0bd0d06817fdb2a1430
Alexandra Ann DeLucia <aadelucia@lanl.gov>
2019-02-13 19:08:46 -0700

    added more explanatory comments

--- week_5/count_tokens.py
+++ week_5/count_tokens.py
@@ -13,8 +13,21 @@ import re
 # and sanitize for analysis
 # "words" are called "tokens"
 def tokenize_file(file):
+    """
+    Tokenizes a given file.
+    
+    "tokenizing" refers to stripping the symbols from a text, transforming
+    all words to lower case, and separating them into a list.
+    
+    Parameters
+    ----------
+    file : path to a plain text file to tokenize
+    
+    Returns
+    -------
+    list : list of all tokens in file
+    """
     # Open the file
-
     
     # Clean all symbols from the text
     # Hint: how did we clean the text for our palindrome method?
@@ -27,12 +40,24 @@ def tokenize_file(file):
     # separated by a single space
     
     # Convert words to lower case
+    # Use .lower() on each string in the list
+    # Use a list comprehension to access each word
     
+    # Return word list
     return
 
 
 # 2. Define a function to count tokens
 def count_tokens_in_file(file):
+    """
+    Parameters
+    ----------
+    file : path to a plain text file to count the tokens
+    
+    Returns
+    -------
+    dict : a dictionary of tokens and their counts {token: count}
+    """
     # Convert file into a list of words using our 
     # tokenize_file method
     
@@ -41,13 +66,14 @@ def count_tokens_in_file(file):
     
     # Loop through the token list and update token counts
     
+    # Return token/count structure
     return
 
 
 # Separate our main from the rest of the file
 if __name__ == "__main__":
     # 1. Get the raw token counts
-    token_counts_dict = count_tokens_in_file("")
+    token_counts_dict = count_tokens_in_file("mary_oliver_summer_day.txt")
     
     # 2. Answer the following questions
     
