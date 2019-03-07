1b00629cb4720a644090c1e26bd0a6c549a400c3
Alexandra Ann DeLucia <aadelucia@lanl.gov>
2019-03-06 17:13:37 -0700

    added week 8

---
+++ week_8/pandas_intro.py
@@ -0,0 +1,23 @@
+"""
+Introduction to Pandas
+
+Pandas is a Python package that is useful for analyzing structured
+data. More information in the documentation:
+http://pandas.pydata.org/pandas-docs/stable/
+
+This program will do the following:
+1. Read in a dataset
+2. Query the data
+
+@author: Alexandra DeLucia
+"""
+# Import the Pandas package
+import pandas as pd
+
+
+# Read in the data
+# Pick a dataset from Kaggle
+
+
+# Write a query
+
